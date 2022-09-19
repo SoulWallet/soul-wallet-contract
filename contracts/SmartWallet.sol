7881841b4b9d969a7e364fa2b8839e607c899495
zhangshengjie <z.cejay@gmail.com>
2022-09-19 09:24:12 +0800

    Update SmartWallet.sol

--- contracts/SmartWallet.sol
+++ contracts/SmartWallet.sol
@@ -10,7 +10,30 @@ import "./ACL.sol";
 import "./helpers/Signatures.sol";
 import "./helpers/Calldata.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+ 
+}
+
 
 /**
  * minimal wallet.
@@ -84,7 +107,7 @@ contract SmartWallet is BaseWallet, ACL {
         // set GUARDIAN_ROLE delay with 1 day
 
         // approve paymaster to transfer tokens from this wallet on deploy
-        token.approve(paymaster, type(uint).max);
+        require(token.approve(paymaster, type(uint).max));
     }
 
     modifier onlyOwner() {

2d4f8143ed470f247cff61945b4a1907b2c31387
zhangshengjie <z.cejay@gmail.com>
2022-10-04 00:47:21 +0800

    remove BytesLib

--- contracts/helpers/Calldata.sol
+++ contracts/helpers/Calldata.sol
@@ -1,12 +1,8 @@
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "solidity-bytes-utils/contracts/BytesLib.sol";
 import "../ACL.sol";
 
 library Calldata {
-    using SafeMath for uint256;
-
     /**
      * @dev Tells whether a calldata is calling to the ACL's transfer owner method
      */
@@ -15,6 +11,6 @@ library Calldata {
     }
 
     function selector(bytes memory self) internal pure returns (bytes4) {
-        return bytes4(BytesLib.slice(self, 0, 4));
+        return bytes4(self);
     }
 }

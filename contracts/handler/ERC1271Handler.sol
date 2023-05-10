a8ea8b27ea5dd3152bcd7878406457f0f806d186
“davidinsuomi” <davidinsuomi@gmail.com>
2023-05-11 01:11:38 +0800

    1271 handler

--- contracts/handler/ERC1271Handler.sol
+++ contracts/handler/ERC1271Handler.sol
@@ -2,17 +2,28 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "../authority/OwnerAuth.sol";
 
 
-contract ERC1271Handler is IERC1271 {
+abstract contract ERC1271Handler is IERC1271, OwnerAuth {
     // bytes4(keccak256("isValidSignature(bytes32,bytes)")
     bytes4 internal constant MAGICVALUE = 0x1626ba7e;
+    bytes4 internal constant InvalidID = 0xffffffff;
 
     function isValidSignature(
         bytes32 hash,
         bytes memory signature
     ) external view override returns (bytes4 magicValue) {
-        //#TODO
-        return MAGICVALUE;
+        if (_isOwner(ECDSA.recover(hash, signature))) {
+            return MAGICVALUE;
+        }
+        bytes32 _hash = ECDSA.toEthSignedMessageHash(hash);
+        address recovered = ECDSA.recover(_hash, signature);
+        if (_isOwner(recovered)) {
+            return MAGICVALUE;
+        } else {
+            return InvalidID;
+        }
     }
 }
--- contracts/interfaces/IERC165.sol
+++ contracts/interfaces/IERC165.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
 interface IERC165 {
     /**
      * @dev Returns true if this contract implements the interface defined by

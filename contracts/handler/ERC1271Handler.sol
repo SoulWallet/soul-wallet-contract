6d0acc779db2e2f410421bbd27ea9bcfd925edee
zhangshengjie <z.cejay@gmail.com>
2023-05-11 14:29:25 +0800

    Update ERC1271Handler.sol

--- contracts/handler/ERC1271Handler.sol
+++ contracts/handler/ERC1271Handler.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "../authority/OwnerAuth.sol";
 import "../helper/SignatureValidator.sol";
 import "../../account-abstraction/contracts/core/Helpers.sol";
 
-abstract contract ERC1271Handler is IERC1271, OwnerAuth, SignatureValidator {
+abstract contract ERC1271Handler is IERC1271, SignatureValidator {
     // bytes4(keccak256("isValidSignature(bytes32,bytes)")
     bytes4 internal constant MAGICVALUE = 0x1626ba7e;
     bytes4 internal constant InvalidID = 0xffffffff;
